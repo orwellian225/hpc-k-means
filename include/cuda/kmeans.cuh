29941e924ad4d942f10eb09c0c1536e5777a5d17
orwellian <orwellian225@gmail.com>
2024-05-21 20:54:06 +0200

    add: cuda funcs to header file

--- include/cuda/kmeans.cuh
+++ include/cuda/kmeans.cuh
@@ -2,6 +2,11 @@
 
 #include "support.hpp"
 
+void handle_cuda_error(cudaError_t error);
+
+__device__ float d_vec_distance(float *vec_1, float *vec_2, uint8_t dimension);
+__global__ void classify(cudaTextureObject_t points, float *centroids, uint32_t *classifications);
+
 void classify_kmeans(
     const uint8_t dimension, const uint32_t num_points, const uint32_t num_classes,
     const float *points, float *centroids, uint32_t *classes,
